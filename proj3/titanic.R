6f284dcdd1d434eb973703fad4609168ac01df3f
Oliwia Kamińska <kaminskaoliwia32@gmail.com>
2025-05-27 16:29:01 +0200

    Projekt grupowy

--- proj3/titanic.R
+++ proj3/titanic.R
@@ -1,5 +1,10 @@
+install.packages("corrplot")
 
 library(dplyr)
+library(corrplot)
+library(tidyverse)
+library(summarytools)
+library(corrplot)
 
 # Wczytanie danych
 titanic <- read.csv("C:\\Users\\kamin\\Desktop\\studia_repo\\programR\\proj3\\titanic_new.csv")
@@ -8,36 +13,11 @@ names(titanic)
 
 # Usuniêcie danych nieistotnych
 titanic1 = subset(titanic, select = -c(Passenger.Id, Name, Fare, Cabin, Ticket, SibSp, Parch))
-View(titanic1)
-
-# Znalezienie b³êdnych danych AGE
-table(titanic$Survived)
-titanic1 <- titanic1 %>% filter(Survived %in% c(0,1))
-View(titanic1 %>% filter(is.na(Age)))
-
-# Szukanie outlierów
-boxplot(titanic1$Age)
-summary(titanic1$Age)
-
-# Obliczamy œredni¹ wieku osobno dla kobiet i mê¿czyzn
-femaleAge <- mean(titanic1 %>% filter(Sex == "female") %>% pull(Age), na.rm = TRUE)
-maleAge <- mean(titanic1 %>% filter(Sex == "male") %>% pull(Age), na.rm = TRUE)
-
-# Zastêpujemy brak wartoœci œredni¹
-titanic1 <- titanic1 %>%
-  mutate(Age = ifelse(is.na(Age) & Sex == "male", maleAge,
-                      ifelse(is.na(Age) & Sex == "female", femaleAge, Age)))
-View(titanic1)
-
-# EMBARKED
-table(titanic1$Embarked)
-titanic1 <- titanic1 %>% mutate(Embarked = toupper(Embarked))
-titanic1 <- titanic %>% filter(Embarked %in% c("S", "Q", "C"))
-View(titanic1)
+names(titanic1)
 
 # SEX
 table(titanic1$Sex)
-titanic1 <- titanic %>%
+titanic1 <- titanic1 %>%
   mutate(Sex = recode(Sex, 
                       "f2emale" = "female",
                       "feemale" = "female",
@@ -46,3 +26,81 @@ titanic1 <- titanic %>%
                       "malle" = "male",
                       "malwe" = "male"))
 table(titanic1$Sex)
+
+# Znalezienie b³êdnych danych SURVIVED
+table(titanic1$Survived)
+titanic1 <- titanic1 %>% filter(Survived %in% c(0,1))
+
+# EMBARKED
+table(titanic1$Embarked)
+titanic1 <- titanic1 %>% mutate(Embarked = toupper(Embarked))
+titanic1 <- titanic1 %>% filter(Embarked %in% c("S", "Q", "C"))
+table(titanic1$Embarked)
+
+# AGE
+View(titanic1 %>% filter(is.na(Age)))
+names(titanic1)
+
+# Szukanie outlierów
+boxplot(titanic1$Age,
+        main = "Czêstotliwoœæ wystêpowania wieku",
+        ylab = "Wiek pasa¿erów",
+        col = "thistle1")
+
+hist(titanic1$Age,
+     breaks = 20,
+     col = "thistle1",
+     main = "Czêstotliwoœæ wystêpowania wieku",
+     xlab = "Wiek pasa¿erów")
+summary(titanic1$Age)
+
+Q1 <- quantile(titanic1$Age, 0.25, na.rm = TRUE)
+Q3 <- quantile(titanic1$Age, 0.75, na.rm = TRUE)
+
+IQR <- Q3 - Q1
+
+lower_bound <- Q1 - 1.5 * IQR
+upper_bound <- Q3 + 1.5 * IQR
+
+titanic1_nO <- titanic1 %>% filter(titanic1$Age >= lower_bound & titanic1$Age<= upper_bound)
+names(titanic1)
+
+# TITANIC1 Obliczamy œredni¹ wieku osobno dla kobiet i mê¿czyzn
+femaleAge <- mean(titanic1 %>% filter(Sex == "female") %>% pull(Age), na.rm = TRUE)
+maleAge <- mean(titanic1 %>% filter(Sex == "male") %>% pull(Age), na.rm = TRUE)
+
+# Zastêpujemy brak wartoœci œredni¹
+titanic1 <- titanic1 %>%
+  mutate(Age = ifelse(is.na(Age) & Sex == "male", maleAge,
+                      ifelse(is.na(Age) & Sex == "female", femaleAge, Age)))
+
+# TITANIC1_NO
+femaleAge_nO <- mean(titanic1_nO %>% filter(Sex == "female") %>% pull(Age), na.rm = TRUE)
+maleAge_nO <- mean(titanic1_nO %>% filter(Sex == "male") %>% pull(Age), na.rm = TRUE)
+
+# Zastêpujemy brak wartoœci œredni¹
+titanic1_nO <- titanic1_nO %>%
+  mutate(Age = ifelse(is.na(Age) & Sex == "male", maleAge,
+                      ifelse(is.na(Age) & Sex == "female", femaleAge, Age)))
+
+# Porównujemy œrednie dla zawartoœci z outlierami i bez
+print(femaleAge)
+print(maleAge)
+print(femaleAge_nO)
+print(maleAge_nO)
+
+
+# PCLASS
+table(titanic2$Pclass)
+
+# ANALIZA DANYCH
+
+# Statystyki opisowe
+summary(titanic2$Age)
+
+shapiro.test(titanic2$Age)
+cor.test(titanic2$Age, titanic2$Survived, method = "spearman")
+cor.test(titanic2$Age, titanic2$Pclass, method = "spearman")
+cor.test(titanic2$Survived, titanic2$Pclass, method = "spearman")
+
+
---
+++ proj_grup/sklep_proj3.R
@@ -0,0 +1,92 @@
+
+# Przygotowanie modelu zale¿noœci sprzeda¿y od  zale¿noœci od liczby klientów.
+# Ile wynosi teoretyczna sprzeda¿ w dniach, w których liczba klientów bêdzie wynosi³a 599,616.
+
+library(dplyr)
+library(corrplot)
+library(readxl)
+
+# Wczytanie danych
+sklep <- read_excel("sklep77.xlsx")
+head(sklep)
+names(sklep)
+
+# Preprocessing
+# Jedyne dane, które s¹ nam potrzebne to te odnosz¹ce siê do liczby klientów
+# oraz iloœci sprzeda¿y, warto te¿ usun¹æ dni w których sklep by³ zamkniêty
+# dodatkowo takie dane jak data, dzien tyogodnia, wystepowanie swieta sa dla nas nieistotne
+# a sklep id niepotrzebne bo omawiamy tylko jeden sklep
+
+sklep = subset(sklep, select = -c(sklep_id, data, czy_promocja, czy_swieto_szkolne,
+                                  sklep_asort, dzien_tyg, czy_swieto, sklep_typ, sklep_konkurencja))
+
+names(sklep)
+
+# Czy otwarty
+table(sklep$czy_otwarty)
+sklep <- sklep %>% filter(czy_otwarty %in% c("Tak"))
+table(sklep$czy_otwarty)
+
+# Liczba klientów
+boxplot(sklep$liczba_klientow,
+        main = "Klienci",
+        col = "thistle",
+        ylab = "Liczba klientów")
+
+# Usuniêcie outlierów
+Q1_lk <- quantile(sklep$liczba_klientow, 0.25, na.rm = TRUE)
+Q3_lk <- quantile(sklep$liczba_klientow, 0.75, na.rm = TRUE)
+IQR_lk <- Q3_lk - Q1_lk
+lower_lk <- Q1_lk - 1.5 * IQR_lk
+upper_lk <- Q3_lk + 1.5 * IQR_lk
+
+sklep1 <- sklep %>% filter(sklep$liczba_klientow >= lower_lk & sklep$liczba_klientow<= upper_lk)
+
+hist(sklep$liczba_klientow,
+     main = "Liczba klientów - przed usuniêciem outlierów",
+     col = "thistle1")
+
+hist(sklep1$liczba_klientow,
+     main = "Liczba klientów - po usuniêciu outlierów",
+     col = "thistle1")
+
+# Sprzeda¿
+boxplot(sklep$sprzedaz,
+        main = "Sprzeda¿",
+        col = "thistle",
+        ylab = "Przychód")
+
+# Usuniêcie outlierów
+Q1_s <- quantile(sklep1$sprzedaz, 0.25, na.rm = TRUE)
+Q3_s <- quantile(sklep1$sprzedaz, 0.75, na.rm = TRUE)
+IQR_s <- Q3_s - Q1_s
+lower_s <- Q1_s - 1.5 * IQR_s
+upper_s <- Q3_s + 1.5 * IQR_s
+
+sklep2 <- sklep1 %>% filter(sprzedaz >= lower_s & sprzedaz <= upper_s)
+
+
+hist(sklep$sprzedaz,
+     main = "Sprzeda¿ - przed usuniêciem outlierów",
+     col = "thistle1")
+
+hist(sklep2$sprzedaz,
+     main = "Sprzeda¿ - po usuniêciu outlierów",
+     col = "thistle1")
+
+# Statystyki 
+summary(sklep2$liczba_klientow)
+summary(sklep2$sprzedaz)
+
+# Regresja liniowa
+model <- lm(sprzedaz ~ liczba_klientow, data = sklep2)
+summary(model)
+
+# Wykres
+plot(sklep2$liczba_klientow, sklep2$sprzedaz, pch = 16, col = "skyblue",
+     xlab = "Liczba klientów", ylab = "Sprzeda¿")
+abline(model, col = "red", lwd = 2)
+
+# Przewidzenie wyników dla liczby klientów = 599 oraz = 616
+przewidywanie <- data.frame(liczba_klientow = c(599, 616))
+predict(model, newdata = przewidywanie)
