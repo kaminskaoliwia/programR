bd7155e6632c82f58e868abce5e1602e62176fcd
Oliwia <kaminskaoliwia32@gmail.com>
2026-07-24 12:31:55 +0200

    Fix file path in CSV read function

--- zapytaniaCD/zapytaniaCD.R
+++ zapytaniaCD/zapytaniaCD.R
@@ -2,6 +2,6 @@
 # 3. Wybierz wszystkie kolumny zawieraj¹ce ci¹g znaków "arr"
 
 library(dplyr)
-flights <- read.csv("\zapytaniaCD\nycflights.csv")
+flights <- read.csv("nycflights.csv")
 flights_arr <- flights %>% select(contains("arr"))
 names(flights_arr)
